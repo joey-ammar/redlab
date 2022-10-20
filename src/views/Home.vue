1729584b1103e7df33b37a7f3192934f2470fd8f
joey <hammar@micropayment.de>
2022-10-20 15:26:34 +0200

    Fontawesome added + the credentials page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <script setup>
-  
+
 </script>
 
 <template>
-   <div class="main d-flex flex-column justify-content-center align-items-center">
-        <h1>Willkomen TO REDLAB</h1>
-   </div>>
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <h1>Red<font-awesome-icon class="red" :icon="['fas', 'flask']" />Lab</h1>
+        <h1>You Have the Access Token credentials</h1>
+    </div>
+    
 </template>
 
 <style scoped lang="scss">
 
     .main {
         height: 100vh;
-        background-color: #eaedef;
+        background-color: #eaedef;                                                                                                                           
+    }
+
+    .red {
+        color: #dd594a;
     }
 
 </style>
