c6a37bb08b99a4a0857fb2b84d1594a68defdaac
joey <hammar@micropayment.de>
2022-10-20 15:27:02 +0200

    Added fontaweomse + credentails

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import BootstrapVue3 from 'bootstrap-vue-3'
-import "bootstrap/dist/css/bootstrap.css";
 
 import App from './App.vue'
 import router from './router'
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faFlask } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret, faFlask)
+
 
 import './assets/main.css'
 
@@ -13,6 +20,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(BootstrapVue3)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
 
 
