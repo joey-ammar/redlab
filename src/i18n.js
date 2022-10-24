3eb02c35392602202f46b7d023e35eca78d5021b
joey <hammar@micropayment.de>
2022-10-24 15:29:37 +0200

    Translation works !!!

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,22 @@
-import { createI18n } from 'vue-i18n';
-import en from './locals/en.json';
-import de from './locals/de.json';
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+    en: {
+        hello: 'hello world'
+    },
+    ja: {
+        hello: 'こんにちは、世界'
+    }
+}
 
 const i18n = createI18n({
-    legacy: false,
-    locale: 'en',
-    globalInjection: true,
-    fallbackLocale: 'en',
-    messages: {
-        en,
-        de,
-    },
-});
+    legacy: false, // you must set `false`, to use Composition API
+    locale: messages, // set locale
+    fallbackLocale: 'en', // set fallback locale
+    messages, // set locale messages
+    // If you need to specify other options, you can set other options
+    // ...
+  })
+
 
 export default i18n;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,23 @@
 <script setup>
-    import {ref} from "vue"
-    import i18n from '../i18n.js';
-
-    let t = i18n.global.t;
-
-    const selected = ref('en')
-
-    const options = ref([
-        { text: 'English', value: 'en' },
-        { text: 'Deutsch', value: 'de' },
-    ])
+    import i18n from '../i18n';
+    const {t, locale, messages} = i18n.global;
+    let lang = {
+        en: locale.value = 'en',
+        ja: locale.value = 'ja'
+    }
 
 </script>
 
 <template>
     <div class="main d-flex flex-column justify-content-center align-items-center">
 
-        <select v-model="selected">
-            <option v-for="option in options" :value="option.value">
-                {{ option.text }}
-            </option>
+        <select v-model="locale">
+            <option :value="lang.en">en</option>
+            <option :value="lang.ja">ja</option>
         </select>
 
-        <div>Selected: {{ selected }}</div>
+        {{t('hello')}}
 
-        <h1>{{t('hi')}}</h1> 
-        {{selected}}
         <h1>Red<font-awesome-icon class="red" :icon="['fas', 'flask']" />Lab</h1>
         <h1>You Have the Access Token credentials</h1>
     </div>
