632282c421b431838199a7506e3acefee2bc34c4
joey <hammar@micropayment.de>
2022-10-24 08:15:38 +0200

    added select

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,10 @@
 
 <template>
     <div class="main d-flex flex-column justify-content-center align-items-center">
+        <select>
+            <option>English</option>
+            <option>Deutsch</option>
+        </select>
         <h1>Red<font-awesome-icon class="red" :icon="['fas', 'flask']" />Lab</h1>
         <h1>You Have the Access Token credentials</h1>
     </div>
@@ -12,9 +16,16 @@
 
 <style scoped lang="scss">
 
+    select {
+        position: absolute;
+        top: 4rem;
+        right: 4rem;
+    }
+
     .main {
         height: 100vh;
-        background-color: #eaedef;                                                                                                                           
+        background-color: #eaedef;   
+        position: relative;                                                                                                                        
     }
 
     .red {
